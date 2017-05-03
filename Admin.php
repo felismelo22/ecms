c1da7270a53d2d18df2563a52dc30259ac1897d3
iwan safrudin <iwan@fisip.net>
2017-05-03 08:44:46 +0700

    update database and admin controller

---
+++ Admin.php
@@ -0,0 +1,188 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+	public function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('url');
+    $this->load->helper('html');
+    $this->load->helper('email');
+    $this->load->model('admin_model');
+    $this->load->model('content_model');
+		$this->load->library('session');
+		$this->load->library('pagination');
+  }
+	public function index()
+	{
+		$this->load->view('admin/index');
+	}
+
+  public function login()
+  {
+    if(!empty($_POST))
+    {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $allow = $this->admin_model->login($username,$password);
+      if(!empty($allow))
+      {
+        $user_data = array(
+                      'id' => $allow['id'],
+                      'username'=> $allow['username']
+                      );
+        $this->session->set_userdata('logged_in', $user_data);
+
+        redirect(base_url('admin'));
+      }else{
+        $data['msg'] = 'pastikan username dan password anda benar';
+        $data['alert'] = 'danger';
+        $this->load->view('admin/index', $data);
+      }
+    }else{
+      $this->load->view('admin/index');
+    }
+  }
+	public function logout()
+  {
+    $this->session->sess_destroy();
+    redirect(base_url('admin'));
+  }
+
+  /*USER*/
+	public function user_list($page = 0, $keyword = NULL)
+	{
+    if(!empty($_POST['del_user']))
+    {
+      $this->admin_model->del_user($_POST['del_user']);
+      $data['msg']   = 'data berhasil dihapus';
+      $data['alert'] = 'success';
+    }
+
+    $data = $this->admin_model->get_all_user($page, $keyword);
+		$this->load->view('admin/index',$data);
+	}
+  public function user_edit($id = 0)
+  {
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $this->form_validation->set_rules('username', 'Username', 'required');
+    $this->form_validation->set_rules('password', 'Password', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+      $data['msg'] = '';
+      $data['alert'] = '';
+      if($id != 0)
+      {
+        $data['data_user']  = $this->admin_model->get_user($id);
+      }
+      $this->load->view('admin/index', $data);
+    }else{
+      $username = $this->input->post('username');
+      $email = $this->input->post('email');
+      if(valid_email($email))
+      {
+        if($id > 0)
+        {pr($_POST);
+          $this->admin_model->set_user($id);
+          $data['data_user']  = $this->admin_model->get_user($id);
+        }else{
+          $user = $this->admin_model->get_user($username);
+          $data['msg'] = 'Success Saving Data';
+          $data['alert'] = 'success';
+          if($user['username'] == $username)
+          {
+            $data['msg'] = 'Failed Saving Data, username exist';
+            $data['alert'] = 'danger';
+          }
+          $this->admin_model->set_user();
+        }
+      }else{
+        $data['msg'] = 'Please insert valid email';
+        $data['alert'] = 'danger';
+      }
+      $this->load->view('admin/index', $data);
+    }
+  }
+
+  /*CONTENT*/
+  public function content_category($id = 0)
+  {
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+      $data['msg']           = '';
+      $data['alert']         = '';
+      $data['parent']        = $this->content_model->get_cat_ids();
+      $data['data_cat_list'] = $this->content_model->get_cat_list();
+      $parent                = array();
+    // pr($this->db->last_query());die();
+
+      foreach ($data['parent'] as $key => $value)
+      {
+        $parent[$value['id']] = $value['title'];
+      }
+      $parent[0] = 'None';
+      $data['parent'] = $parent;
+      if($id != 0)
+      {
+        $data['data_cat']  = $this->content_model->get_cat($id);
+      }
+      $this->load->view('admin/index', $data);
+    }else{
+      $title  = $this->input->post('title');
+      $par_id = $this->input->post('par_id');
+      if($id > 0)
+      {
+        $row = $this->content_model->get_cat_row('WHERE id = '.$id);
+        if($title != $row['title'])
+        {
+          $cat_row       = $this->content_model->get_cat_row('WHERE par_id = '.$par_id.' AND title = "'.$title.'"');
+          $data['msg']   = 'Failed Saving Data, title exist';
+          $data['alert'] = 'danger';
+
+          if(empty($cat_row))
+          {
+            $data['msg'] = 'Success Saving Data';
+            $data['alert'] = 'success';
+            $this->content_model->set_cat($id);
+          }
+        }
+        // $data['data_cat']  = $this->content_model->get_cat($id);
+
+        $this->content_model->set_user();
+      }else{
+
+        $cat_row       = $this->content_model->get_cat_row('WHERE par_id = '.$par_id.' AND title = "'.$title.'"');
+        $data['msg']   = 'Failed Saving Data, title exist';
+        $data['alert'] = 'danger';
+
+        if(empty($cat_row))
+        {
+          $data['msg'] = 'Success Saving Data';
+          $data['alert'] = 'success';
+          $this->content_model->set_cat();
+        }
+      }
+
+      $data['parent']        = $this->content_model->get_cat_ids();
+      $data['data_cat_list'] = $this->content_model->get_cat_list();
+      $parent                = array();
+
+      foreach ($data['parent'] as $key => $value)
+      {
+        $parent[$value['id']] = $value['title'];
+      }
+      $parent[0] = 'None';
+      $data['parent'] = $parent;
+
+      $this->load->view('admin/index', $data);
+    }
+  }
+}
--- database.sql
+++ database.sql
@@ -7,39 +7,69 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+DROP TABLE IF EXISTS `content_cat`;
+CREATE TABLE `content_cat` (
+  `id` int(11) NOT NULL,
+  `par_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `publish` tinyint(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `content_cat` (`id`, `par_id`, `title`, `description`, `publish`, `created`, `updated`) VALUES
+(1, 0, 'Uncategorized', 'this is uncategorized', 1, '2017-05-01 07:33:31', '2017-05-01 07:33:31'),
+(2, 0, 'kategori a', 'test', 1, '2017-05-02 06:46:43', '2017-05-02 06:47:42'),
+(3, 0, 'kategori b', 'test', 1, '2017-05-02 06:48:14', '2017-05-02 06:48:14'),
+(4, 0, 'kategori c', 'test', 1, '2017-05-02 06:49:32', '2017-05-02 06:49:32'),
+(5, 0, 'kategori d', '', 1, '2017-05-02 06:52:06', '2017-05-02 06:52:06'),
+(6, 1, 'unkategori a', '', 1, '2017-05-02 06:52:23', '2017-05-02 06:52:23'),
+(7, 1, 'unkategori b', '', 1, '2017-05-02 06:55:46', '2017-05-02 06:55:46'),
+(8, 2, 'kategori a', '', 1, '2017-05-02 07:02:35', '2017-05-02 07:02:35'),
+(9, 0, 'testing unpublish', '', 1, '2017-05-03 07:02:08', '2017-05-03 07:02:08');
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` tinyint(1) NOT NULL DEFAULT '5' COMMENT '1=admin, 2=editor, 3=author, 4=contributor, 5=subscriber',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`id`, `username`, `password`, `created`, `updated`) VALUES
-(1, 'admin', '123456', '2017-04-04 09:55:29', '2017-04-04 09:55:29'),
-(2, 'iwan', 'md5(123456)', '2017-04-05 08:45:41', '2017-04-05 08:45:41'),
-(3, 'fredi', 'md5(123456)', '2017-04-05 08:46:49', '2017-04-05 08:46:49'),
-(4, 'lina', 'md5(123456)', '2017-04-05 08:46:55', '2017-04-05 08:46:55'),
-(5, 'pae', 'md5(123456)', '2017-04-05 08:47:02', '2017-04-05 08:47:02'),
-(6, 'mae', 'md5(123456)', '2017-04-05 08:47:07', '2017-04-05 08:47:07'),
-(7, 'bue', 'md5(123456)', '2017-04-05 08:47:13', '2017-04-05 08:47:13'),
-(8, 'bapak', 'md5(123456)', '2017-04-05 08:47:19', '2017-04-05 08:47:19'),
-(9, 'ulfa', 'md5(123456)', '2017-04-05 08:47:25', '2017-04-05 08:47:25'),
-(10, 'coba_password', 'e10adc3949ba59abbe56e057f20f883e', '2017-04-06 10:18:06', '2017-04-06 10:18:06'),
-(11, 'coba', '0cc175b9c0f1b6a831c399e269772661', '2017-04-07 08:00:09', '2017-04-07 08:00:09'),
-(12, 'iwan', 'e10adc3949ba59abbe56e057f20f883e', '2017-04-11 07:30:12', '2017-04-11 07:30:12'),
-(13, 'iwan', '13bbf54a6850c393fb8d1b2b3bba997b', '2017-04-11 07:31:05', '2017-04-11 07:31:05'),
-(14, 'iwan', '13bbf54a6850c393fb8d1b2b3bba997b', '2017-04-11 07:31:16', '2017-04-11 07:31:16'),
-(15, 'iwan', 'e10adc3949ba59abbe56e057f20f883e', '2017-04-11 07:34:24', '2017-04-11 07:34:24');
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `role`, `created`, `updated`) VALUES
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'iwan@fisip.net', 1, '2017-04-04 09:55:29', '2017-05-01 06:42:58'),
+(2, 'iwan', 'md5(123456)', '', 5, '2017-04-05 08:45:41', '2017-04-05 08:45:41'),
+(3, 'fredi', 'md5(123456)', '', 5, '2017-04-05 08:46:49', '2017-04-05 08:46:49'),
+(4, 'lina', 'md5(123456)', '', 5, '2017-04-05 08:46:55', '2017-04-05 08:46:55'),
+(5, 'pae', 'md5(123456)', '', 5, '2017-04-05 08:47:02', '2017-04-05 08:47:02'),
+(6, 'mae', 'md5(123456)', '', 5, '2017-04-05 08:47:07', '2017-04-05 08:47:07'),
+(7, 'bue', 'md5(123456)', '', 5, '2017-04-05 08:47:13', '2017-04-05 08:47:13'),
+(8, 'bapak', 'md5(123456)', '', 5, '2017-04-05 08:47:19', '2017-04-05 08:47:19'),
+(9, 'ulfa', 'md5(123456)', '', 5, '2017-04-05 08:47:25', '2017-04-05 08:47:25'),
+(10, 'coba_password', 'e10adc3949ba59abbe56e057f20f883e', '', 5, '2017-04-06 10:18:06', '2017-04-06 10:18:06'),
+(11, 'coba', '0cc175b9c0f1b6a831c399e269772661', '', 5, '2017-04-07 08:00:09', '2017-04-07 08:00:09'),
+(12, 'iwan', 'e10adc3949ba59abbe56e057f20f883e', '', 5, '2017-04-11 07:30:12', '2017-04-11 07:30:12'),
+(13, 'iwan', '13bbf54a6850c393fb8d1b2b3bba997b', '', 5, '2017-04-11 07:31:05', '2017-04-11 07:31:05'),
+(14, 'iwan', '13bbf54a6850c393fb8d1b2b3bba997b', '', 5, '2017-04-11 07:31:16', '2017-04-11 07:31:16'),
+(15, 'iwan', 'e10adc3949ba59abbe56e057f20f883e', '', 5, '2017-04-11 07:34:24', '2017-04-11 07:34:24'),
+(16, 'frediansah', 'e10adc3949ba59abbe56e057f20f883e', '', 5, '2017-05-01 05:53:56', '2017-05-01 05:53:56');
 
+
+ALTER TABLE `content_cat`
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 
+ALTER TABLE `content_cat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
